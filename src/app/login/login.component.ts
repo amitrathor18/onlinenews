2c3d60932adaf75b646a41f93410448d37266b6b
amitrathor18 <amitrathor181293@gmail.com>
2020-09-03 02:46:09 +0530

    login completed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,21 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 // import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import{faUser} from '@fortawesome/free-solid-svg-icons'
-// import{faGoogle} from'@fortawesome/free-brands-svg-icons'
+// import { faUser } from '@fortawesome/free-solid-svg-icons';
+// import { faLock } from '@fortawesome/free-solid-svg-icons';
+
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 import { from } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  // public faGoogle = faGoogle;
-  
-  constructor() { }
+  // // public faGoogle = faGoogle;
+  // public faUser = faUser;
+  // public faLock = faLock;
+  public uiInvalidCredential = false;
 
-  ngOnInit(): void {
+  public fbFormGroup = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {}
+
+  // async loginProcessHere() {
+  //   const data = this.fbFormGroup.value;
+
+  //   // ajax call check the local mysql api to  verify username and password
+  //   const url = 'http://localhost:3000/auth-user';
+  //   const result: any = await this.http.post(url, data).toPromise();
+  //   if (result.opr) {
+  //     sessionStorage.setItem('sid', 'true');
+  //     this.router.navigate(['home']);
+  //   } else {
+  //     this.uiInvalidCredential = true;
+  //   }
+  // }
+  loginProcessHere() {
+    const data = this.fbFormGroup.value;
+
+    if (data.username === 'amit' && data.password === 'admin') {
+      this.router.navigate(['home']);
+    } else {
+      this.uiInvalidCredential = true;
+    }
   }
-
 }
