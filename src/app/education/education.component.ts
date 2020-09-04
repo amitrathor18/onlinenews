0b643147d7635e49736a4ad1073092b9eda10cc2
amitrathor18 <amitrathor181293@gmail.com>
2020-09-05 03:38:21 +0530

    forget and contact us added sucessfully

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class EducationComponent implements OnInit {
   public imgsrc = 'assets/me.jpg';
 
-  public list = [{ img: 'assets/n17.png' }];
+  
 
   constructor() {}
 
